31a94cd5a665f559631dd3edae1bf37c68100fe4
ccandy <ccunandy2001@gmail.com>
2022-02-14 19:32:05 -0800

    add unity project

---
+++ Assets/Scripts/MeshCombiner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+
+
+public class MeshCombiner : MonoBehaviour
+{
+
+    private void Start()
+    {
+        CombineMesh();
+    }
+
+
+
+
+    public void CombineMesh()
+    {
+
+    }
+}
